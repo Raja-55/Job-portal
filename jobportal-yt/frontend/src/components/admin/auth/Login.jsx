2d13d7a908e78bf813ad7aba60e8ff5189171254
Raja-55 <contactraja44@gmail.com>
2026-02-03 21:25:04 +0530

    "change navber"

--- jobportal-yt/frontend/src/components/admin/auth/Login.jsx
+++ jobportal-yt/frontend/src/components/admin/auth/Login.jsx
@@ -1,5 +1,4 @@
 import  { useEffect, useState } from 'react'
-import Navbar from '../../shared/Navbar'
 import { Label } from '../../ui/label'
 import { Input } from '../../ui/input'
 import { RadioGroup } from '../../ui/radio-group'
@@ -55,7 +54,7 @@ const Login = () => {
     },[])
     return (
         <div>
-            <Navbar />
+
             <div className='flex items-center justify-center max-w-7xl mx-auto'>
                 <form onSubmit={submitHandler} className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
                     <h1 className='font-bold text-xl mb-5'>Login</h1>
